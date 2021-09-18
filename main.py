b26289e719109c9095f909429d82a07d7bc1b0d7
KateIvul <caterina.acce@gmail.com>
2021-09-18 11:41:36 +0200

    Update main

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from TabuSearch import *
 
 if __name__=="__main__" :
 
-    test = TSPReader(r"C:\Users\Cater\OneDrive\Desktop\UNIVERSITA\RO\ELABORATO 2.0\ISTANZE DI PROVA\eil51.tsp")
-    test1 = TabuSearch(test.matrixDistance , test.coords)
+    #test = TSPReader(r"C:\Users\Cater\OneDrive\Desktop\UNIVERSITA\RO\ELABORATO 2.0\ISTANZE DI PROVA\eil51.tsp")
+    #test1 = TabuSearch(test.matrixDistance , test.coords)
+    test_Reader = TSPReader("eil51.tsp" , "eil51.opt.tour.txt")
+    test_TabuSearch = TabuSearch(test_Reader.matrixDistance , test_Reader.coords)
 
-    test_coordinate = test.coords
-    test_matrix = test.matrixDistance
+    test_coordinate = test_Reader.coords
+    test_matrix = test_Reader.matrixDistance
     
-    test1_s0 = test1.initial_solution
-    print("Random initial solution = " , test1_s0)
+    test1_s0 = test_TabuSearch.initial_solution
+    print("Random initial solution = " , test1_s0 , "\n")
